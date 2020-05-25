7a6eb4375ffcd65918ce4ba2703ab0c9639a8cb6
crowley <crowley@instructure.com>
2020-05-25 17:22:57 -0600

    adding activestorage.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_024029) do
+ActiveRecord::Schema.define(version: 2020_05_25_230804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "work_period"
+    t.boolean "show"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.text "skill"
