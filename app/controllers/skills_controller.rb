b7a222557512e60ad5eef9324abeaaf28f7e7fa8
crowley <crowley@instructure.com>
2020-06-07 15:36:27 -0600

    now showing skills. Kind of.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,5 @@
 class SkillsController < ApplicationController
-    
+    before_action :set_skill, only: [:edit, :update, :show, :destroy]
     def index
     end
 
@@ -9,10 +9,23 @@ class SkillsController < ApplicationController
 
     def create
         @skill = Skill.new(skill_params)
+        if @skill.save
+            redirect_to @skill
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    
     end
 
     private
     def skill_params
         params.require(:skill).permit(:skill, :show)
+    end
+    
+    def set_skill
+        @skill = Skill.find(params[:id])
     end
 end
