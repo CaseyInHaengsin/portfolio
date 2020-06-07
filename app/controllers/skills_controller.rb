295a437d20b5c3ce1ebc337a5282c4b29fd83ef7
crowley <crowley@instructure.com>
2020-06-07 15:30:47 -0600

    I have the form rendering.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,11 +1,18 @@
 class SkillsController < ApplicationController
+    
     def index
     end
 
     def new
-    
+     @skill = Skill.new
     end
 
     def create
+        @skill = Skill.new(skill_params)
+    end
+
+    private
+    def skill_params
+        params.require(:skill).permit(:skill, :show)
     end
 end
