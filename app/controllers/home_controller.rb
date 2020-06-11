76c4aeb205be161ff85fe002376b2c306ccd55da
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-06-11 16:38:56 -0600

    added first part of home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
     #skip_before_action :authenticate_user!  
     def index
+        @experiences = Experience.where(show: true).paginate(page: params[:page], per_page: 20)
+        #@experiences = Experience.all.where(show: true)
+        @educations = Education.all.where(show: true)
     end
 
     def create
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,2 +1,5 @@
 class Home < ApplicationRecord
+    has_many :skills
+    has_many :educations
+    has_many :educations
 end
